8d8bcaa8061aa2e423fe3a0c05d20b4fb9093193
Tiago <contabeis.tiago@gmail.com>
2026-03-23 10:58:07 -0300

    fix firebase auth

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,13 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage"; // 👈 IMPORTANTE
+import { getStorage } from "firebase/storage";
+import { getAuth } from "firebase/auth"; // 👈 FALTAVA ISSO
 
 const firebaseConfig = {
   apiKey: "AIzaSyC4aX32zOl04SSWDv6hl_zJy7VpMkXCtjo",
   authDomain: "sistema-laboratorio-vet.firebaseapp.com",
   projectId: "sistema-laboratorio-vet",
-  storageBucket: "sistema-laboratorio-vet.appspot.com", // 👈 TEM QUE TER
+  storageBucket: "sistema-laboratorio-vet.appspot.com",
   messagingSenderId: "913558014260",
   appId: "1:913558014260:web:994215bbd2f4a10743d61"
 };
@@ -14,4 +15,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
-export const storage = getStorage(app); // 👈 ESSENCIAL
+export const storage = getStorage(app);
+export const auth = getAuth(app); // 👈 ESSA LINHA RESOLVE TUDO
