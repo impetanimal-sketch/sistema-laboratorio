4e5145aec27d777ef75e4172e0e62483bf40739c
Tiago <contabeis.tiago@gmail.com>
2026-03-23 10:32:28 -0300

    fix useEffect auth

--- src/App.js
+++ src/App.js
@@ -45,7 +45,7 @@ function App() {
     onAuthStateChanged(auth, (usuario) => {
       setUser(usuario);
     });
-  }, []);
+  }, [auth]);
 
   // 🔄 carregar exames
   const carregarExames = async () => {
